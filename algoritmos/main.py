0f7574f2d054510276f7405020a3ccf6b41a68ea
laurarmoran <44478508+laurarmoran@users.noreply.github.com>
2018-10-31 15:16:20 +0100

    Add files via upload

--- algoritmos/main.py
+++ algoritmos/main.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env/python
 
-import sys
-
-
 from read_csv import read_csv
 from a_star_ import a_star_
-# from a_star_ import Node
 
 # from dijkstra import dijkstra
 
 
-def choose_algorithm():  ## Your menu design here
+def choose_algorithm():
     print('Please choose an algorithm: ')
     print('1. A* (A star)')
     print('2. Dijkstra')
-
     choice = int(input("Enter your choice [1-2]: "))
 
     while 1:
@@ -29,47 +24,8 @@ def choose_algorithm():  ## Your menu design here
 
 
 def choose_map():
-
-    print('What map do yoy want to use? [1-9]')
-
-    choice = int(input('Enter your choice [1-9]: '))
-    mapa = ''
-
-    while 1:  # While loop which will keep going until loop = False
-
-        if choice == 1:
-            print('Map 1 selected')
-            mapa = 'map1.csv'
-        elif choice == 2:
-            print('Map 2 selected')
-            mapa = 'map2.csv'
-        elif choice == 3:
-            print('Map 3 selected')
-            mapa = 'map3.csv'
-        elif choice == 4:
-            print('Map 4 selected')
-            mapa = 'map4.csv'
-        elif choice == 5:
-            print('Map 5 selected')
-            mapa = 'map5.csv'
-        elif choice == 6:
-            print('Map 6 selected')
-            mapa = 'map6.csv'
-        elif choice == 7:
-            print('Map 7 selected')
-            mapa = 'map7.csv'
-        elif choice == 8:
-            print('Map 8 selected')
-            mapa = 'map8.csv'
-        elif choice == 9:
-            print('Map 9 selected')
-            mapa = 'map9.csv'
-        else:
-            # Any integer inputs other than values 1-5 we print an error message
-            input('Wrong option selection. Enter any key to try again...')
-        break
-
-    return mapa
+    choice = int(input('What map do yoy want to use? [1-9]: '))
+    return choice
 
 
 def main():
@@ -79,7 +35,6 @@ def main():
     path_a_star_ = a_star_(reader, start, end)
 
     print('PATH:')
-
     print(path_a_star_)
 
 
--- algoritmos/read_csv.py
+++ algoritmos/read_csv.py
@@ -4,20 +4,48 @@ import csv
 import numpy
 
 
-def read_csv(mapa):
+def read_csv(choice):
 
+    mapa = 'map'+str(choice)+'.csv'
     with open(mapa) as myFile:
         reader = csv.reader(myFile)
         reader = list(reader)   # convert tuple to list (list of strings)
         maze = []       # list of lists
 
-        start = (1, 1)
-        end = (7, 3)
-
         print('\nMAP: ')
 
-        for row in reader:
+        for row in reader:   # -------> row pone columnas, line pone filas
             maze.append(list(map(int, row)))        # convert to list of lists (lists of integers instead of strings)
         print(numpy.array(maze))
 
+        start, end = set_origin(maze)
+
     return maze, start, end
+
+
+def set_origin(maze):
+
+    while 1:
+        print('\nSet origin: ')
+        x_orig = int(input('X_orig: '))
+        y_orig = int(input('Y_orig: '))
+
+        if maze[x_orig][y_orig] != 0:
+            input('\nInvalid origin coordinates. Press any key to select new ones... ')
+            continue
+        else:
+            start = (x_orig, y_orig)
+            print('start: ', start)
+
+        print('\nSet destination: ')
+        x_dest = int(input('X_dest: '))
+        y_dest = int(input('Y_dest: '))
+
+        if maze[x_dest][y_dest] != 0:
+            input('\nInvalid destination coordinates. Press any key to select new ones... ')
+            continue
+        else:
+            end = (x_dest, y_dest)
+            print('end: ', end)
+
+        return start, end
